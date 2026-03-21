afd7a3ced3edc1984af63135aa3b26ee9b4f561c
GoldeneToilette <saasbusfahrer@gmail.com>
2026-03-21 03:21:22 +0100

    placed function in wrong module

--- server/dispatchers/wallet.lua
+++ server/dispatchers/wallet.lua
@@ -1,5 +1,6 @@
 local errors = requireC("/GuardLink/server/lib/errors.lua")
 local message = requireC("/GuardLink/server/network/message.lua")
+local audit = requireC("/GuardLink/server/lib/audit.lua")
 
 local handlers = {}
 
@@ -265,6 +266,49 @@ function handlers.remove_member(msg, client, id, ctx, fn, logger)
 
     local msg = message.create("wallet", {
         action = "remove_member", status = "success"
+    }, client.aesKey, false, id)
+    session:send(client.channel, msg)
+    return 0
+end
+
+function handlers.audit(msg, client, id, ctx, fn, logger)
+    if not client then return 0 end
+    local session = ctx.services["network_session"]
+
+    local name = msg.payload.wallet
+    local wallets = ctx.services["wallets"]
+    local wallet = wallets:getWalletData(name)
+
+    if not wallet then
+        local msg = message.create("wallet", {
+            action = "audit",
+            status = "failure",
+            error = errors.WALLET_NOT_FOUND.client
+        }, client.aesKey, false, id)
+        session:send(client.channel, msg)
+        return 0
+    end
+
+    local ethic = ctx.configs["identity"].selectedEthic
+    local ethics = ctx.configs["rules"].rules.ethics[ethic].values
+    local logsAccessible = ctx.configs["rules"].server.formulas.logsAccessible
+    local hasPermission = ctx.services["accounts"]:hasPermission(client.account, "wallets.view_others")
+
+    if not isMember(wallet, client.account) and not hasPermission and not logsAccessible(ethics.autonomy, ethics.consent) then
+        local msg = message.create("wallet", {
+            action = "audit",
+            status = "failure",
+            error = errors.INSUFFICIENT_PERMISSIONS.client
+        }, client.aesKey, false, id)
+        session:send(client.channel, msg)
+        return 0
+    end
+
+    local entries = audit.get("wallets", name, ctx.services["vfs"])
+    local msg = message.create("wallet", {
+        action = "audit",
+        status = "success",
+        data = entries
     }, client.aesKey, false, id)
     session:send(client.channel, msg)
     return 0
